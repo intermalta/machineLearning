ba10e1989df9585b3a7dd3f39c86355257205a23
intermalta <intermalta@gmail.com>
2014-10-20 00:24:01 -0200

    Multi cost calculation and gradient descent for multiples features

--- machine-learning/mlclass-ex1-007/mlclass-ex1/gradientDescentMulti.m
+++ machine-learning/mlclass-ex1-007/mlclass-ex1/gradientDescentMulti.m
@@ -16,19 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+	theta_aux = zeros(size(theta),1);
+	for i = 1:size(theta)
+	  theta_aux(i) = theta(i) - alpha * (1/m) * sum((X*theta-y).*X(:,i));
+	  printf('%d\n', theta_aux(i));
+	end
 
     % ============================================================
-
+	theta = theta_aux;
     % Save the cost J in every iteration    
     J_history(iter) = computeCostMulti(X, y, theta);
 
